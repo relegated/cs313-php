01c6b3179f13100e1ac6bae9975c38b3707ff39b
relegated <35969849+relegated@users.noreply.github.com>
2020-01-25 11:18:10 -0700

    Update 03shoppingCartView.php

--- web/individual/03shoppingCartView.php
+++ web/individual/03shoppingCartView.php
@@ -33,6 +33,8 @@ $_SESSION['statue'] = serialize($statue);
 $_SESSION['paperTowels'] = serialize($paperTowel);
 $_SESSION['ark'] = serialize($ark);
 
+setlocale(LC_MONETARY, 'en_US.UTF-8');
+
 ?>
 
 <html>
@@ -50,44 +52,44 @@ $_SESSION['ark'] = serialize($ark);
     <span><img src="../images/bagOfGold.png" alt="Bag of Gold" height="50" width="50"></span>
     <label class="qty" for="bagOfGold">qty<label>
     <?php echo '<input type="number" id="bagOfGold" name="bagOfGoldQty" min="0" max="5" value="'.$bagOfGold->qty.'"disabled>'?>
-    <span class="price">$2,000</span>
-    <?php echo '<span class="total">'.$bagOfGold->total.'</span>'?>
+    <span class="price">/unit: $2,000</span>
+    <?php echo '<span class="total"> Total: '.money_format("%.2n", floatval($bagOfGold->total)).'</span>'?>
 </li>
 <li>
     <span class="identifier">Two-Headed Cobra</span>
         <span><img src="../images/twoHeadedCobra.png" alt="Two-headed Cobra" height="50" width="50"></span>
     <label class="qty" for="twoHeadedCobra">qty<label>
     <?php echo '<input type="number" id="twoHeadedCobra" name="twoHeadedCobraQty" min="0" max="5" value="'.$twoHeadedCobra->qty.'"disabled>'?> 
-    <span class="price">$700</span>
-    <?php echo '<span class="total">'.$twoHeadedCobra->total.'</span>'?>
+    <span class="price">/unit: $700</span>
+    <?php echo '<span class="total"> Total: '.money_format("%.2n", floatval($twoHeadedCobra->total)).'</span>'?>
 </li>
 <li><span class="identifier">300 lb Bag of Bean Burritos</span>
     <span><img src="../images/burritos.png" alt="Burritos" height="50" width="50"></span>
     <label class="qty" for="burritos">qty<label>
     <?php echo '<input type="number" id="burritos" name="burritosQty" min="0" max="5" value="'.$burrito->qty.'"disabled>'?>
-    <span class="price">$200</span>
-    <?php echo '<span class="total">'.$burrito->total.'</span>'?>
+    <span class="price">/unit: $200</span>
+    <?php echo '<span class="total"> Total: '.money_format("%.2n", floatval($burrito->total)).'</span>'?>
 </li>
 <li><span class="identifier">Life-size Statue of Andre the Giant made of Swiss Cheese</span>
     <span><img src="../images/statue.png" alt="Life-size Statue of Andre the Giant made of Swiss Cheese" height="50" width="50"></span>
     <label class="qty" for="statue">qty<label>
     <?php echo '<input type="number" id="statue" name="statueQty" min="0" max="5" value="'.$statue->qty.'"disabled>'?> 
-    <span class="price">$3,812</span>
-    <?php echo '<span class="total">'.$statue->total.'</span>'?>
+    <span class="price">/unit: $3,812</span>
+    <?php echo '<span class="total"> Total: '.money_format("%.2n", floatval($statue->total)).'</span>'?>
 </li>
 <li><span class="identifier">Roll of Paper Towels</span>
     <span><img src="../images/paperTowels.png" alt="Roll of Paper Towels" height="50" width="50"></span>
     <label class="qty" for="paperTowels">qty<label>
     <?php echo '<input type="number" id="paperTowels" name="paperTowelsQty" min="0" max="5" value="'.$paperTowel->qty.'"disabled>'?> 
-    <span class="price">$2</span>
-    <?php echo '<span class="total">'.$paperTowel->total.'</span>'?>
+    <span class="price">/unit: $2</span>
+    <?php echo '<span class="total"> Total: '.money_format("%.2n", floatval($paperTowel->total)).'</span>'?>
 </li>
 <li class="disabled"><span class="identifier">Ark of the Covenant (Out of Stock)</span>
     <span><img src="../images/ark.png" alt="Ark of the Covenant" height="50" width="50"></span>
     <label class="qty" for="ark">qty<label>
     <input type="number" id="ark" min="0" max="5" value="0" disabled> 
-    <span class="price">$2,000,000,000</span>
-    <?php echo '<span class="total">'.$ark->total.'</span>'?>
+    <span class="price">/unit: $2,000,000,000</span>
+    <?php echo '<span class="total"> Total: '.money_format("%.2n", floatval($ark->total)).'</span>'?>
 </li>
     
 </ul>
